0ad70cb8fe15fcfa0068f7f997a7dbfd42b01746
Muhammad-Moiz39 <chmoiz764@gmail.com>
2026-09-02 00:15:24 +0500

    slick slider

--- js/custom.js
+++ js/custom.js
@@ -8,8 +8,24 @@ const toggleMenu = () => {
 
 
 
-$('.about-cards').slick({
+$('#reviews-slider').slick({
   infinite: true,
-  slidesToShow: 1,
+  slidesToShow: 3,
   slidesToScroll: 1,
+  prevArrow: ".prev",
+  nextArrow: ".next",
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
 });

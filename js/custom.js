57d3f024e87b4b246e265e4cd152f832002d343b
Muhammad-Moiz39 <chmoiz764@gmail.com>
2026-09-02 19:57:34 +0500

    residense-card slider

--- js/custom.js
+++ js/custom.js
@@ -29,3 +29,26 @@ $('#reviews-slider').slick({
     }
   ]
 });
+
+$('#residense-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+})
